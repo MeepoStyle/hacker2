cf6dd55d83c5dd833531fdaf4149deb230f06d2c
MeepoStyle <anyelopd33@gmail.com>
2025-04-13 22:31:31 -0500

    Update setup.sh
    ...

--- setup.sh
+++ setup.sh
@@ -3,14 +3,7 @@
 # Author: Juan Rivas (aka @r1vs3c)
 # Modified: Javier Roldan (K4ysuh)
 # Changes:
-# Added 4 new colour schemas
-# New p10k for both user and root without round style
-# Powerline style for kitty and different background and opacity
-# New Polybar design with color-switch enable so you can switch between the schemes to match your wal.
-# New .zshrc without including all of the oh-my-zsh stuff.
-# Auto install FZF for user
-# Auto install NVchad for both user and root
-# Auto install sudo-plugin for zsh
+# Instalación de Neofetch desde Git en vez de APT para evitar errores
 
 # Colours
 greenColour="\e[0;32m\033[1m"
@@ -57,12 +50,25 @@ else
 	sleep 1
 	echo -e "\n\n${blueColour}[*] Installing necessary packages for the environment...\n${endColour}"
 	sleep 2
-	sudo apt install -y kitty rofi feh xclip ranger i3lock-fancy scrot scrub wmname imagemagick cmatrix htop neofetch python3-pip procps tty-clock fzf lsd bat pamixer flameshot
+	sudo apt install -y kitty rofi feh xclip ranger i3lock-fancy scrot scrub wmname imagemagick cmatrix htop python3-pip procps tty-clock fzf lsd bat pamixer flameshot
 	if [ $? != 0 ] && [ $? != 130 ]; then
 		echo -e "\n${redColour}[-] Failed to install some packages!\n${endColour}"
 		exit 1
 	else
 		echo -e "\n${greenColour}[+] Done\n${endColour}"
+		sleep 1.5
+	fi
+
+	# Instalar Neofetch desde GitHub en vez de APT
+	echo -e "\n${purpleColour}[*] Installing Neofetch from GitHub...\n${endColour}"
+	sleep 2
+	git clone https://github.com/dylanaraps/neofetch.git ~/tools/neofetch
+	sudo ln -sf ~/tools/neofetch/neofetch /usr/local/bin/neofetch
+	if [ $? != 0 ] && [ $? != 130 ]; then
+		echo -e "\n${redColour}[-] Failed to install Neofetch from GitHub!\n${endColour}"
+		exit 1
+	else
+		echo -e "\n${greenColour}[+] Neofetch installed from GitHub\n${endColour}"
 		sleep 1.5
 	fi
 
@@ -77,41 +83,7 @@ else
 		sleep 1.5
 	fi
 
-	echo -e "\n${blueColour}[*] Starting installation of necessary dependencies for the environment...\n${endColour}"
-	sleep 0.5
 
-	echo -e "\n${purpleColour}[*] Installing necessary dependencies for bspwm...\n${endColour}"
-	sleep 2
-	sudo apt install -y build-essential git vim libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev libuv1-dev
-	if [ $? != 0 ] && [ $? != 130 ]; then
-		echo -e "\n${redColour}[-] Failed to install some dependencies for bspwm!\n${endColour}"
-		exit 1
-	else
-		echo -e "\n${greenColour}[+] Done\n${endColour}"
-		sleep 1.5
-	fi
-
-	echo -e "\n${purpleColour}[*] Installing necessary dependencies for polybar...\n${endColour}"
-	sleep 2
-	sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
-	if [ $? != 0 ] && [ $? != 130 ]; then
-		echo -e "\n${redColour}[-] Failed to install some dependencies for polybar!\n${endColour}"
-		exit 1
-	else
-		echo -e "\n${greenColour}[+] Done\n${endColour}"
-		sleep 1.5
-	fi
-
-	echo -e "\n${purpleColour}[*] Installing necessary dependencies for picom...\n${endColour}"
-	sleep 2
-	sudo apt install -y meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
-	if [ $? != 0 ] && [ $? != 130 ]; then
-		echo -e "\n${redColour}[-] Failed to install some dependencies for picom!\n${endColour}"
-		exit 1
-	else
-		echo -e "\n${greenColour}[+] Done\n${endColour}"
-		sleep 1.5
-	fi
 
 	echo -e "\n${blueColour}[*] Starting installation of the tools...\n${endColour}"
 	sleep 0.5
